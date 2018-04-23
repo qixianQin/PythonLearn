d6a654d93e3fbfdd8397d1a2fbff34e4a0a4d1ce
qixianQin <381145574@qq.com>
2018-04-23 23:39:50 +0800

    requests 扩展
    requests 扩展

---
+++ SSLTest.py
@@ -0,0 +1,35 @@
+# -*- coding:utf-8 -*-
+# import requests 
+# response = requests.get('https://www.12306.cn')
+# print(response.status_code)
+
+# 证书验证错误
+# requests.exceptions.SSLError: HTTPSConnectionPool(host='www.12306.cn', port=443):
+#  Max retries exceeded with url: / (Caused by SSLError(SSLError("bad handshake: 
+# Error([('SSL routines', 'tls_process_server_certificate', 'certificate verify failed')],)",),))
+
+import requests 
+response = requests.get('https://www.12306.cn', verify= False)
+print(response.status_code)
+
+# D:\softInstall\Python\lib\site-packages\urllib3\connectionpool.py:858: InsecureRequestWarning: Unverified HTTPS request is being made. Adding certificate verification is strongly advised. See: https://urllib3.readthedocs.io/en/latest/advanced-usage.html#ssl-warnings
+#   InsecureRequestWarning)    警告
+# 200
+
+
+import requests 
+from requests.packages import urllib3 
+
+urllib3.disable_warnings()
+response = requests.get('https://www.12306.cn', verify=False)
+print(response.status_code)
+
+#   去掉警告
+# 200   
+
+#  通过捕获警告到日志的方式的警告
+import logging 
+import requests 
+logging.captureWarnings(True) 
+response = requests.get('https://www.12306.cn', verify=False)
+print(response.status_code)
--- requestsTest.py
+++ requestsTest.py
@@ -327,4 +327,54 @@ headers = {
 }
 
 response = requests.get('http://www.zhihu.com', headers=headers)
-print(response.text)     #   直接输出有乱码，需要编码处理（还未找到方法）
+# print(response.text)     #   直接输出有乱码，需要编码处理（还未找到方法）
+
+
+
+import requests 
+Cookies = 'AFAA7JKAwQmPThoxN5nN2BhBHWynl49NW4Q=|1460384539"; _ga=GA1.2.1363499149.1448896119; q_c1=962ff655c163401d87cf449f327500bf|1508034178000|1448896073000; _zap=da857144-ab77-43b3-a83c-4119d443a6f9; z_c0="2|1:0|10:1521036654|4:z_c0|92:Mi4xVF9xM0FBQUFBQUFBVUFEc2tvREJDU1lBQUFCZ0FsVk5ibnVXV3dCMFgtUU14cERERE1rLThIVUtYUmluLVhBTTln|250a65a2539b1db1d6473e152aeef93594f64e637ade6b69fe8bfbadbc26d045"; __DAYU_PP=yayiifbQnbjy2nRA2JfZ211caaa4632a; __utmz=51854390.1521474560.1.1.utmcsr=baidu|utmccn=(organic)|utmcmd=organic; __utmv=51854390.100-1|2=registration_date=20141228=1^3=entry_date=20141228=1; q_c1=962ff655c163401d87cf449f327500bf|1524327443000|1448896073000; _xsrf=c7cdfeaa085c68b5b5a985840f642c54; aliyungf_tc=AQAAABYfrFu05g0AUjUieUy2UacwJ9Wj; __utmc=51854390; __utma=51854390.1363499149.1448896119.1524364788.1524366741.5'
+jar = requests.cookies.RequestsCookieJar()
+headers = {
+	'Host':'www.zhihu.com',
+	'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.162 Safari/537.36',
+	'Upgrade-Insecure-Requests':'1',
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+	'Accept-Encoding': 'gzip, deflate, br',
+	'Accept-Language': 'zh-CN,zh;q=0.9',
+	'Cache-Control': 'max-age=0',
+	'Connection': 'keep-alive'	
+}
+
+for cookie in Cookies.split(';'):
+	key, value = cookie.split('=',1)
+	jar.set(key, value)
+response = requests.get('http://www.zhihu.com', cookies=jar, headers=headers)
+# print(response.text)
+
+
+
+
+import requests 
+requests.get('http://httpbin.org/cookies/set/number/123456789')
+response = requests.get('http://httpbin.org/cookies')
+print(response.text)
+
+
+# {
+#   "cookies": {
+#     "number": "123456789"
+#   }
+# }
+
+
+import requests 
+s = requests.Session()
+s.get('http://httpbin.org/cookies/set/number/123456789')
+response = s.get('http://httpbin.org/cookies')
+print(response.text)
+
+# {
+#   "cookies": {
+#     "number": "123456789"
+#   }
+# }
