5a6ba62b400776b2e6b4560eb045f7bdc8b98017
qixianQin <381145574@qq.com>
2018-04-28 00:10:24 +0800

    完善爬取猫眼电影爬虫
    完善爬取猫眼电影爬虫

--- eyeOfCatMovie.py
+++ eyeOfCatMovie.py
@@ -1,16 +1,25 @@
 # -*- coding:utf-8 -*- 
 
 ####  爬取猫眼电影 TOP100 
+####  需要加上请求的头，headers  不然会被拦截
 
 import requests 
 import re
+import json
+import time 
+from requests.exceptions import RequestException
 
+###  请求打开每一页， 并返回页面源码
 def get_one_page(url, headers):
-	response = requests.get(url, headers=headers)
-	if response.status_code == 200:
-		return response.text
-	return None
-
+	try:
+		response = requests.get(url, headers=headers)
+		if response.status_code == 200:
+			return response.text
+		return None
+	except RequestException as e:
+		return None 
+	
+#####   正则表达式处理 每一页的信息， 提取需要的信息出来
 def parse_one_page(html):
 	pattern = re.compile('<dd.*?board-index.*?>(.*?)</i>.*?data-src="(.*?)".*?title="(.*?)".*?star">(.*?)</p>.*?releasetime">(.*?)</p>.*?integer">(.*?)</i>.*?fraction">(.*?)</i>.*?</dd>', re.S)
 	items = re.findall(pattern, html)
@@ -19,20 +28,21 @@ def parse_one_page(html):
 			'index':item[0],
 			'image':item[1],
 			'title':item[2].strip(),
-			'actor':item[3].strip()[3:] if len(item[3]) > 3 else '',
-			'time':item[4].strip()[5:] if len(item[4]) > 5 else '',
+			'actor':item[3].strip()[3:] ,
+			'time':item[4].strip()[5:] ,
 			'score':item[5].strip() + item[6].strip()
 		}
-	print(items)	
+	# print(items)	
 
+####  将提取到的信息  写入文件
 def write_to_json(content):
-	with open('result.txt', 'a') as f:
-		print(type(json.dumps(content)))
-		f.write(json.dumps(content, ensure_ascii=False).encode('utf-8'))
+	with open('MovieResult.txt', 'a', encoding='utf-8') as f:
+		# print(type(json.dumps(content)))
+		f.write(json.dumps(content, ensure_ascii=False)+"\n")
 
 
-def main():
-	url = 'http://maoyan.com/board/4'
+def main(offset):
+	url = 'http://maoyan.com/board/4?offset=' + str(offset)
 	headers = {
 		'Cookies':'uuid=1A6E888B4A4B29B16FBA1299108DBE9CB97C6757ABDCAA16811B266DFBAFFC7F; _csrf=311a27007890d9cdab74e1a956f516802f92fc1dd96315e12494d1815315b3c5; _lxsdk_cuid=1630797eb4dc8-0ddb919f9e5c34-3a614108-100200-1630797eb4dc8; _lxsdk=1A6E888B4A4B29B16FBA1299108DBE9CB97C6757ABDCAA16811B266DFBAFFC7F; __mta=110359994.1524840786871.1524840878550.1524841300835.4; _lxsdk_s=1630797eb4f-106-6e9-4fe%7C%7C8',
 		'Host':'maoyan.com',
@@ -46,11 +56,16 @@ def main():
 		'Connection': 'keep-alive'	
 	}
 	html = get_one_page(url, headers)
-	# print(html)
-	parse_one_page(html)
+	for item in parse_one_page(html):
+		print(item)
+		write_to_json(item)
 
 
-main()
+
+if __name__ == '__main__':
+	for i in range(10):
+		main(offset= i * 10)
+		time.sleep(1)
 
 
-####  需要加上请求的头，headers  不然会被拦截
+
